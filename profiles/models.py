825843a82f325309307b3384bce3841180c14f17
Onursoyar <soyaronur@gmail.com>
2023-03-05 15:31:04 +0000

    Create comment and post model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "DRAFT"), (1, "Published"))
+
+class Post(models.Model):
+    title = models.CharField(max_length=220, unique=True)
+    slug = models.SlugField(max_length=220, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="profile_posts")
+    eatured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='profile_likes', blank=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=80)
+    created_on = models.DateTimeField(auto_now_add=True)
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        """The meta class for comments"""
+        ordering = ['created_on']
+
+    def __str__(self):
+        """The comment str return"""
+        return f"Comment {self.body} by {self.name}"
--- talented/settings.py
+++ talented/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'profiles', 
-
 ]
 
 MIDDLEWARE = [
@@ -132,7 +131,8 @@ STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
