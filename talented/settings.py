a2d27aad5c902ba9bc79ea89d594e9b32c204f43
Onursoyar <soyaronur@gmail.com>
2023-04-21 13:21:12 +0000

    fixed secret leak issue

--- talented/settings.py
+++ talented/settings.py
@@ -24,7 +24,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -111,10 +111,6 @@ WSGI_APPLICATION = 'talented.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-if "DATABASE_URL" in os.environ:
-    print('hre is...', os.environ.get("DATABASE_URL"))
-else:
-    print('not exists...')
 
 DATABASES = {'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))}
 
