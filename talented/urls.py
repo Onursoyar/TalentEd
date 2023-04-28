723369ada8112f1ebbe17df7a8639ab453118813
Onursoyar <soyaronur@gmail.com>
2023-04-28 14:36:21 +0500

    Fix django admin UI

--- talented/urls.py
+++ talented/urls.py
@@ -13,12 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from talented import settings
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include("profiles.urls"), name="profiles-urls"),
-    path('summernote/', include('django_summernote.urls')),
-    path('accounts/', include('allauth.urls')),
-]
+                  path('admin/', admin.site.urls),
+                  path("", include("profiles.urls"), name="profiles-urls"),
+                  path('summernote/', include('django_summernote.urls')),
+                  path('accounts/', include('allauth.urls')),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
