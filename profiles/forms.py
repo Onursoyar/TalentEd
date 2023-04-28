62857e25883f7528e1691a968502dfd731e03fbd
Onursoyar <soyaronur@gmail.com>
2023-04-28 14:06:56 +0500

    final push

--- profiles/forms.py
+++ profiles/forms.py
@@ -11,9 +11,4 @@ class CommentForm(forms.ModelForm):
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-<<<<<<< Updated upstream
         fields = ('title',  'featured_image', 'content')
-        
-=======
-        fields = ('title', 'featured_image', 'excerpt', 'content')
->>>>>>> Stashed changes
